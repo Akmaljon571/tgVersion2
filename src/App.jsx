f4839a87b97b02a18a712e7d68b064538f2db7fc
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-06-20 16:14:17 +0500

    boshlangich

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useRef } from 'react';
+import { Header, Main, Footer } from './contect'
+import './App.scss';
+function App() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    )
+}
+
+export default App;
