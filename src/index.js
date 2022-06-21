dacd07f81000c2210e369a5413d07948a3074a20
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-06-21 16:29:38 +0500

    list

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ValueContext, ValueProvide } from './contect/useTil/valueContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ValueProvide>
+      <App />
+    </ValueProvide>
 );
 
